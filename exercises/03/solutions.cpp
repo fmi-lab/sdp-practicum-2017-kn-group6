f45d7518d6ec90b7819a5decdf877502eac19886
Anton Petkov <anton.petkoff@gmail.com>
2017-10-18 01:04:44 +0300

    OddIterator postfix traversal

--- exercises/03/solutions.cpp
+++ exercises/03/solutions.cpp
@@ -183,3 +183,57 @@ TEST_CASE("matching parentheses") {
         CHECK_FALSE(isBalanced("(<>{)}"));
     }
 }
+
+class OddIterator {
+private:
+    int number;
+
+public:
+    OddIterator() : number(1) {}
+
+    OddIterator& operator++() {
+        number += 2;
+        return *this;
+    }
+
+    OddIterator operator++(int) {
+        OddIterator previous = *this;
+        number += 2;
+        return previous;
+    }
+
+    int operator*() {
+        return number;
+    }
+
+    OddIterator& operator->() {
+        return *this;
+    }
+
+    bool operator==(const OddIterator& other) const {
+        return number == other.number;
+    }
+
+    bool operator!=(const OddIterator& other) const {
+        return !(*this == other);
+    }
+
+    operator bool() const { // this iterator is infinite
+        return true;
+    }
+
+    bool operator!() const {
+        return false;
+    }
+};
+
+TEST_CASE("OddIterator") {
+    SUBCASE("traversal with postfix increment") {
+        const int SIZE = 100;
+        OddIterator iter;
+        for (int i = 1; i < SIZE; ++i) {
+            CHECK(*(iter++) == 2 * (i - 1) + 1);
+            CHECK(*iter == 2 * i + 1);
+        }
+    }
+}
