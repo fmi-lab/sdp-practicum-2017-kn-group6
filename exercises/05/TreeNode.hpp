21dd8f1bb56617f22ff9231761f54d2f13d0ed3d
Anton Petkov <anton.petkoff@gmail.com>
2017-11-08 09:21:43 +0200

    Sum binary tree

---
+++ exercises/05/TreeNode.hpp
@@ -0,0 +1,15 @@
+#ifndef TREE_NODE_H
+#define TREE_NODE_H
+
+template <typename T>
+class TreeNode {
+public:
+    T value;
+    TreeNode<T>* left;
+    TreeNode<T>* right;
+
+    TreeNode(const T& _value = T(), TreeNode<T>* _left = nullptr, TreeNode<T>* _right = nullptr)
+            : value(_value), left(_left), right(_right) {}
+};
+
+#endif //TREE_NODE_H
---
+++ exercises/05/sum.cpp
@@ -0,0 +1,24 @@
+#include "../../lib/doctest.h"
+#include "TreeNode.hpp"
+
+int sum(TreeNode<int> *root) {
+    if (root == nullptr) {
+        return 0;
+    }
+
+    return root->value + sum(root->left) + sum(root->right);
+}
+
+typedef TreeNode<int> IntNode;
+
+TEST_CASE ("sum tree") {
+    TreeNode<int> *tree =
+        new IntNode(10,
+             new IntNode(4,
+                 new IntNode(1),
+                 new IntNode(3)),
+             new IntNode(15,
+                 new IntNode(12)));
+
+    CHECK(45 == sum(tree));
+}
